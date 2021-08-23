1bcb0b6ad0e9de2e990a6b7c20b12a44a2a73517
WojcikMikolaj <52234302+WojcikMikolaj@users.noreply.github.com>
2021-08-23 15:41:46 +0200

    Server address changed

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ class WeatherData(Jsonable):
 
 class ServerData:
     port = 4000
-    address = '127.0.0.1'
+    address = '0.0.0.0'
     socket_type_first = socket.AF_INET
     socket_type_second = socket.SOCK_DGRAM
 
